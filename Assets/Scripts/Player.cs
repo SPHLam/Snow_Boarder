575653a75a9568de74bf8cb7d4ffc2e28a5b86b2
SPHLam <samphanhoailam@gmail.com>
2024-06-04 15:16:02 +0700

    Add Torque
    - Force Rotation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D _rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _rb2d.AddTorque(horizontalInput * 10);
+    }
+}
