d6f28b1ddbd012b8be50aa61ea000a45e1272699
SPHLam <samphanhoailam@gmail.com>
2024-06-05 16:05:35 +0700

    Sliding effect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,17 +10,25 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private ParticleSystem _crashEffect;
+
+    [SerializeField]
+    private ParticleSystem _slidingEffect;
+
+    private SurfaceEffector2D _surfaceEffector2D;
+
+    private float _playerSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
+        _surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        _rb2d.AddTorque(horizontalInput * 10);
+        RotatePlayerMovement();
+        SpeedBoostMovement();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -32,8 +40,36 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            _slidingEffect.Play();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            _slidingEffect.Stop();
+        }
+    }
+
     private void ReloadScene()
     {
         SceneManager.LoadScene(0);
+    }
+
+    private void RotatePlayerMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _rb2d.AddTorque(horizontalInput * 10);
+    }
+
+    private void SpeedBoostMovement()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        _surfaceEffector2D.speed = _playerSpeed + verticalInput * 10;
     }
 }
